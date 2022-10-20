492240d6c5252500a713cdc89cc11b5b5cb5412e
eudemonia <173537077@qq.com>
2022-10-20 17:25:18 +0800

    init pro

---
+++ .eslintrc.cjs
@@ -0,0 +1,69 @@
+module.exports = {
+
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    es6: true
+  },
+  extends: [
+    'standard',
+    'standard-react',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended'
+  ],
+  // globals: { Atomics: 'readonly', SharedArrayBuffer: 'readonly' },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      legacyDecorators: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json'
+  },
+  plugins: [
+    'babel',
+    'react',
+    'promise',
+    '@typescript-eslint'
+  ],
+  rules: {
+    camelcase: 0,
+    'react/display-name': ['off', { ignoreTranspilerName: false }],
+    indent: [
+      'error',
+      2,
+      {
+        // ignoredNodes: ['JSXAttribute', 'JSXSpreadAttribute']
+      }
+    ],
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'multiline-ternary': 'off',
+    'react/jsx-curly-brace-presence': [2, { props: 'never', children: 'never' }],
+    'no-empty': ['off', { allowEmptyCatch: true }],
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-var-requires': 0,
+    'react/jsx-indent': [0, 2],
+    semi: 0,
+    'react/no-unused-prop-types': 0,
+    'react/prop-types': 0,
+    'generator-star-spacing': [
+      'error',
+      {
+        before: false,
+        after: true
+      }
+    ],
+    'comma-dangle': ['error', 'only-multiline'],
+    'import/no-named-default': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  },
+};
